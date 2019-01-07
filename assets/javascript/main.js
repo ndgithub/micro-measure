84ad5b778508f00569e6c1a2153cf49096d4e0dd
ndgithub <nickydesai.mail@gmail.com>
2019-01-06 18:10:47 -0600

    Adds calculation for image percent of scalebar width

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -3,6 +3,7 @@
 // beach-small.jpg 272 X 92
 // beach.jpg 5184 X 3456
 // grid.jpg 1000 X 1000
+// steel.png 360 X 240
 
 // Dimensions for #micro-container
 var containerSizeX = $("#micro-container").width();
@@ -22,16 +23,20 @@ var bgPosY;
 
 // Boolean for if the scalebar has been set (to turn off click listener)
 var isScalebarSet;
-// Array for scalebar points
-var clickPoints = [];
+
+// Array for scalebar points relative to #micro-container
+var clickPointsContainer = [];
+
+//Array for scalebar points relative to image
+var clickPointsImage = [];
 
 // Boolean if hovering over #micro-container
 var isHovering;
 
 setInitBgImageSize(); // Determine what size should be and assign to variables
 setInitBgPos(); // Determine what position should be and assign to variables
-updateBgSize(); // Update image size in UI
-updateBgPos();  // Update image pos in UI
+updateUiBgSize(); // updates image size in UI
+updateUiBgPos();  // updates image pos in UI
 
 setHoverListener(); // sets isHovering to true if hovering o #micro-contiainer
 setKeysForZoomListener(); // Set a keypress listener on the body for zoom and scroll
@@ -62,52 +67,53 @@ function setInitBgPos() {
     bgPosY = (containerSizeY - bgSizeY) / 2;
 }
 
-function updateBgSize() {
+function updateUiBgSize() {
     $("#micro-container").css("background-size", Math.floor(bgSizeX) + "px " + Math.floor(bgSizeY) + "px");
 }
 
-function updateBgPos() {
+function updateUiBgPos() {
     $("#micro-container").css("background-position", Math.floor(bgPosX) + "px " + Math.floor(bgPosY) + "px");
-}
-
-function onScalebarSet() {
-    isScalebarSet = true;
-    console.log('isScalebarSet', isScalebarSet);
-    var length = Math.abs(clickPoints[0] - clickPoints[1]);
-    var div = $("<div>");
-    div.append("<br> Length: " + length);
-}
-
-function updateClickPoint() {
-    $("#scalebar-info").html("");
-    for (var i =0; i < 2; i++) {
-        $("#scalebar-info").append(clickPoints[i]);
-        $("#scalebar-info").append("<br> ");
-    };
 }
 
 function resetClickPoints() {
     isScalebarSet = false;
     console.log('isScalebarSet', isScalebarSet);
-    clickPoints = [];
+    clickPointsContainer = [];
+    clickPointsImage = [];
     $("#scalebar-info").html("");
 }
+
+function updateUiClickPoint() {
+    $("#scalebar-info").html("");
+    for (var i =0; i < 2; i++) {
+        $("#scalebar-info").append(clickPointsContainer[i],"<br>");
+    };
+}
+
+function onScalebarSet() {
+    isScalebarSet = true;
+    console.log('isScalebarSet', isScalebarSet);
+    var length = Math.abs(clickPointsContainer[0] - clickPointsContainer[1]);
+    $("#scalebar-info").append(length);
+}
+
+
 
 function setClickforScalebarListener() {
     $("#set-scalebar-button").on("click", function () {
         resetClickPoints();
         $("#micro-container").on("click", function (event) { // 
-            clickPoints.push(event.pageX);
-            updateClickPoint();
-            if (clickPoints.length === 2) {
+            clickPointsContainer.push(event.pageX);
+            var imageXpercent  = ((event.pageX) - bgPosX)/bgSizeX;
+            console.log('imageXpercent', imageXpercent);
+            clickPointsImage.push(imageXpercent);
+            updateUiClickPoint();
+            if (clickPointsContainer.length === 2) {
                 onScalebarSet();
                 $(this).off("click");                
             }
         });
     });
-
-
-
 }
 
 function setKeysForZoomListener() { // Set a keypress listener on the body for zoom and scroll
@@ -170,8 +176,8 @@ function setKeysForZoomListener() { // Set a keypress listener on the body for z
             default:
             // code block
         }
-        updateBgSize();
-        updateBgPos();
+        updateUiBgSize();
+        updateUiBgPos();
         var containerCenterX_relToImgCorner_percent = ((containerSizeX / 2) - bgPosX) / bgSizeX;
         var containerCenterY_relToImgCorner_percent = ((containerSizeY / 2) - bgPosY) / bgSizeY;
         console.log(containerCenterX_relToImgCorner_percent);
